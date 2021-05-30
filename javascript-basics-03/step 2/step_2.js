96ee2cbe1b36042ddee81ffcd7a2253d1ea3f823
Amira-GH <amiraghraiy@gmail.com>
2021-05-30 17:59:28 +0300

    step_2 Done

---
+++ javascript-basics-03/step 2/step_2.js
@@ -0,0 +1,16 @@
+
+function myfunction(){
+    
+
+
+document.querySelector('#show').addEventListener('click',()=>{
+    document.querySelector('#text').hidden = false;
+});
+
+document.querySelector('#hide').addEventListener('click',()=>{
+    document.querySelector('#text').hidden = true;
+
+});
+
+
+}
