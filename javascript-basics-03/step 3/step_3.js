3493835aedb62cc9f3397717f377355c3a87c2a0
Amira-GH <amiraghraiy@gmail.com>
2021-05-30 18:11:00 +0300

    step_3 Done

---
+++ javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,15 @@
+
+
+
+function green(){
+    document.getElementById("text").style.color = "green";
+}
+
+
+function red(){
+    document.getElementById("text").style.color = "red";
+}
+
+function blue(){
+    document.getElementById("text").style.color = "blue";
+}
