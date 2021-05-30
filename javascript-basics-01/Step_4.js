84d1c59c6a6da5fe0077ea91db6c92535f9ff93b
Amira-GH <amiraghraiy@gmail.com>
2021-05-30 13:25:05 +0300

    Step_4 Done

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let name = prompt("What is your name?","name");
+let surname = prompt("What is your surname?","surname");
+let city = prompt("What is your city?","city");
+alert('Hello ' + name  + ' ' + surname + ' from ' + city);
